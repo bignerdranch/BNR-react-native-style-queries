f91b71fc01fa00e61b45181a434e79a1802ce661
Josh Justice <me@codingitwrong.com>
2021-12-26 07:26:22 -0500

    Pass test by changing the property value conditionally

--- src/useStyleQueries.js
+++ src/useStyleQueries.js
@@ -1,7 +1,11 @@
 function useStyleQueries(styleConfig) {
   const entries = Object.entries(styleConfig);
   const transformedEntries = entries.map(([key, value]) => {
-    return [key, value];
+    if (Array.isArray(value)) {
+      return [key, {}];
+    } else {
+      return [key, value];
+    }
   });
   const result = Object.fromEntries(transformedEntries);
   return result;

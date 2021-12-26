cacf35b92f07a956e00a636f1967f25f7a91788c
Josh Justice <me@codingitwrong.com>
2021-12-26 07:25:47 -0500

    Refactor to prepare to transform object values

--- src/useStyleQueries.js
+++ src/useStyleQueries.js
@@ -1,5 +1,10 @@
 function useStyleQueries(styleConfig) {
-  return styleConfig;
+  const entries = Object.entries(styleConfig);
+  const transformedEntries = entries.map(([key, value]) => {
+    return [key, value];
+  });
+  const result = Object.fromEntries(transformedEntries);
+  return result;
 }
 
 module.exports = useStyleQueries;
